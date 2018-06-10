0b0900740b142a43a973b81e36acb6809ec3278f
LarisaGl <89888985265@mail.ru>
2018-06-10 18:55:44 +0300

    int

--- inheritance__and_interface.php
+++ inheritance__and_interface.php
@@ -1,142 +1,183 @@
 <?php
 
-echo "1. Полиморфизм- это взаимозаменяемость объектов с одинаковым классом или интерфейсом. Полиморфизмом является свойство унаследованных классов иметь одинаковые методы, которые будут работать по-разному в контексте объектов. Полиморфизм позволяет родительском классу определять общее поведение всех своих потомков. Мы используем один и тот же код для объектов разных классов, в последствии потомки могут переопределять эти методы, т.е. задавать индивидуальное поведение данных методов."."<br>"."Наследование - это свойство классов расширяться. Дочерний класс наследует все public и protected методы и объекты родительского класса. Эти методы и объекты могут быть переопределены, или они могут сохранять свое исходное значение. В дочерний класс можно добавлять дополнительные объекты и методы, тем самым расширяя функциональность класса-родителя."."<br>";
+    echo "1. Полиморфизм- это взаимозаменяемость объектов с одинаковым классом или интерфейсом. Полиморфизмом является свойство унаследованных классов иметь одинаковые методы, которые будут работать по-разному в контексте объектов. Полиморфизм позволяет родительском классу определять общее поведение всех своих потомков. Мы используем один и тот же код для объектов разных классов, в последствии потомки могут переопределять эти методы, т.е. задавать индивидуальное поведение данных методов."."<br>"."Наследование - это свойство классов расширяться. Дочерний класс наследует все public и protected методы и объекты родительского класса. Эти методы и объекты могут быть переопределены, или они могут сохранять свое исходное значение. В дочерний класс можно добавлять дополнительные объекты и методы, тем самым расширяя функциональность класса-родителя."."<br>";
 
-echo "2. Интерфейс похож на класс, но он не может содержать код. Интерфейс может определять имена методов и аргументов, но не содержание методов. Один класс может реализовать несколько интерфейсов. Также и абстрактные классы могут содержать лишь описание абстрактных методов. Абстрактные методы не имеют тела или реализации (как интерфейсы), они лишь описывают, что должен уметь делать объект, а как он это будет делать – проблема наследников абстрактного класса."."<br>"."В абстрактном классе можно объявлять и обычные методы, которые могут быть унаследованы. Интерфейс, в отличие от абстрактного класса, не может содержать методы, имеющие реализацию (public) – он описывает только чистый функционал в виде методов, которые должны реализовать его наследники."."<br>"."В интерфейсах нет наследования. В интерфейсах есть функционал имплементации."."<br>"."Если в классе, который реализует интерфейс, не реализованы все методы интерфейса, то он должен быть абстрактным. Соответственно, интерфейс может быть 'надстройкой' абстрактного класса."."<br>"."Интерфейс нужен обычно когда описывается только протокол (порядок) взаимодействия. А абстрактный класс нужен, когда нужно семейство классов, у которых есть много общего."."<br>";
+    echo "2. Интерфейс похож на класс, но он не может содержать код. Интерфейс может определять имена методов и аргументов, но не содержание методов. Один класс может реализовать несколько интерфейсов. Также и абстрактные классы могут содержать лишь описание абстрактных методов. Абстрактные методы не имеют тела или реализации (как интерфейсы), они лишь описывают, что должен уметь делать объект, а как он это будет делать – проблема наследников абстрактного класса."."<br>"."В абстрактном классе можно объявлять и обычные методы, которые могут быть унаследованы. Интерфейс, в отличие от абстрактного класса, не может содержать методы, имеющие реализацию (public) – он описывает только чистый функционал в виде методов, которые должны реализовать его наследники."."<br>"."В интерфейсах нет наследования. В интерфейсах есть функционал имплементации."."<br>"."Если в классе, который реализует интерфейс, не реализованы все методы интерфейса, то он должен быть абстрактным. Соответственно, интерфейс может быть 'надстройкой' абстрактного класса."."<br>"."Интерфейс нужен обычно когда описывается только протокол (порядок) взаимодействия. А абстрактный класс нужен, когда нужно семейство классов, у которых есть много общего."."<br>";
 
-class Main
-{
-	public $name;
-	public $price;
+    interface mainInt {
+        public function  __construct($name, $price, $discount);
+        public function getPrice ();	
+    }
 
-	public function  __construct($name, $price)
-	{
-		$this->name=$name;
-		$this->price=$price;
-	}
-}
+    interface carInt extends mainInt {
+    	public function setCountry($country);
+    	public function setSpeed($speed);
+    }
 
-class Car extends Main
-{
-	public $country;
-	public $speed;
+    interface tvInt extends mainInt {
+    	public function setSize($size);
+    }
 
-	public function setCountry($country)
-	{
-		$this->country=$country;
-		return $this;		
-	}
+    interface penInt extends mainInt {
+    	public function setColor($color);
+    }
 
-	public function setSpeed($speed)
-	{
-		$this->speed=$speed;
-		return $this;
-	}
-}
+    interface duckInt extends mainInt {
+    	public function setWeight($weight);
+    }
 
-$bmw = new Car ('BMW M3', 7000000);
-$bmw->setCountry('German')->setSpeed(350);
-$mazda = new Car ('Mazda 3',  1500000);
-$mazda->setCountry('Japan')->setSpeed(220);
+    interface prodInt extends mainInt {
+    	public function setSeasons($season);
+    }
 
-echo "<pre>";
-print_r($bmw);
-echo "</pre>";
-echo "<pre>";
-print_r($mazda);
-echo "</pre>";
+    class Main
+    {
+        public $name;
+        public $price;
+        public $discount;
 
-class TV extends Main
-{
-	public $size;
+        public function  __construct($name, $price, $discount)
+        {
+            $this->name=$name;
+            $this->price=$price;
+            $this->discount=$discount;
+        }
 
-	public function setSize($size)
-	{
-		$this->size=$size;
-		return $this;		
-	}
-}
+        public function getPrice () {
+            return round ($this->price - ($this->price * $this->discount / 100));
+        }
+    }
 
-$lg = new TV ('LG', 35000);
-$lg->setSize(40);
-$samsung = new TV ('Samsung', 50000);
-$samsung->setSize(45);
+    class Car extends Main implements carInt
+    {
+        public $country;
+        public $speed;
 
+        public function setCountry($country)
+        {
+            $this->country=$country;
+            return $this;		
+        }
 
-echo "<pre>";
-print_r($lg);
-echo "</pre>";
-echo "<pre>";
-print_r($samsung);
-echo "</pre>";
+        public function setSpeed($speed)
+        {
+            $this->speed=$speed;
+            return $this;
+        }
+    }
 
-class Pen extends Main
-{
-	public $color;
+    $bmw = new Car ('BMW M3', 7000000, 10);
+    $bmw->setCountry('German')->setSpeed(350);
+    echo "<pre>";
+    print_r($bmw);
+    echo "</pre>";
+    echo "Ваша цена:".$bmw->getPrice();
 
-	public function setColor($color)
-	{
-		$this->color=$color;
-		return $this;		
-	}
-}
+    $mazda = new Car ('Mazda 3',  1500000, 15);
+    $mazda->setCountry('Japan')->setSpeed(220);
+    echo "<pre>";
+    print_r($mazda);
+    echo "</pre>";
+    echo "Ваша цена:".$mazda->getPrice();
 
-$colop = new Pen ('Colop', 30);
-$colop->setColor('white');
-$parker = new Pen ('Parker',  1000);
-$parker->setColor('black');
+    class TV extends Main implements tvInt
+    {
+        public $size;
 
-echo "<pre>";
-print_r($colop);
-echo "</pre>";
-echo "<pre>";
-print_r($parker);
-echo "</pre>";
+        public function setSize($size)
+        {
+            $this->size=$size;
+            return $this;		
+        }
+    }
 
-class Duck extends Main
-{
-	public $weight;
+    $lg = new TV ('LG', 35000, 5);
+    $lg->setSize(40);
+    echo "<pre>";
+    print_r($lg);
+    echo "</pre>";
+    echo "Ваша цена:".$lg->getPrice();
 
-	public function setWeight($weight)
-	{
-		$this->weight=$weight;
-		return $weight;		
-	}
-}
+    $samsung = new TV ('Samsung', 50000, 15);
+    $samsung->setSize(45);    
+    echo "<pre>";
+    print_r($samsung);
+    echo "</pre>";
+    echo "Ваша цена:".$samsung->getPrice();
 
-$one = new Duck ('fried', 450);
-$one->setWeight(1500);
-$two = new Duck ('braised', 300);
-$two->setWeight(1000);
+    class Pen extends Main implements penInt
+    {
+        public $color;
 
-echo "<pre>";
-print_r($one);
-echo "</pre>";
-echo "<pre>";
-print_r($two);
-echo "</pre>";
+        public function setColor($color)
+        {
+            $this->color=$color;
+            return $this;		
+        }
+    }
 
-class Product extends Main
-{
-	public $season;
+    $colop = new Pen ('Colop', 30, 3);
+    $colop->setColor('white');
+    echo "<pre>";
+    print_r($colop);
+    echo "</pre>";
+    echo "Ваша цена:".$colop->getPrice();
 
-	public function setSeasons($season)
-	{
-		$this->season=$season;
-		return $season;
-	}
-}
+    $parker = new Pen ('Parker',  1000, 7);
+    $parker->setColor('black');    
+    echo "<pre>";
+    print_r($parker);
+    echo "</pre>";
+    echo "Ваша цена:".$parker->getPrice();
 
-$guess = new Product ('Guess', 15000);
-$guess->setSeasons ("2017/2018");
-$dkny = new Product ('DKNY', 22000);
-$guess->setSeasons ("2018/2019");
+    class Duck extends Main implements duckInt
+    {
+        public $weight;
 
-echo "<pre>";
-print_r($guess);
-echo "</pre>";
-echo "<pre>";
-print_r($dkny);
-echo "</pre>";
+        public function setWeight($weight)
+        {
+            $this->weight=$weight;
+            return $weight;		
+        }
+    }
+
+    $one = new Duck ('fried', 450, 1);
+    $one->setWeight(1500);
+    echo "<pre>";
+    print_r($one);
+    echo "</pre>";
+    echo "Ваша цена:".$one->getPrice();
+
+    $two = new Duck ('braised', 300, 2);
+    $two->setWeight(1000);    
+    echo "<pre>";
+    print_r($two);
+    echo "</pre>";
+    echo "Ваша цена:".$two->getPrice();
+
+    class Product extends Main implements prodInt
+    {
+        public $season;
+
+        public function setSeasons($season)
+        {
+            $this->season=$season;
+            return $season;
+        }
+    }
+
+    $guess = new Product ('Guess', 15000, 20);
+    $guess->setSeasons ("2017/2018");
+    echo "<pre>";
+    print_r($guess);
+    echo "</pre>";
+    echo "Ваша цена:".$guess->getPrice();
+
+    $dkny = new Product ('DKNY', 22000, 25);
+    $guess->setSeasons ("2018/2019");    
+    echo "<pre>";
+    print_r($dkny);
+    echo "</pre>";
+    echo "Ваша цена:".$dkny->getPrice();
 
 ?>
